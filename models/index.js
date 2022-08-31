52641ff22536e932281b8c994e9871d061767508
Sumit Ojha <sumitojha501@gmail.com>
2022-08-30 21:02:00 -0500

    Comment

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require("./User");
 const Post = require("./Post");
+const Comment = require("./Comment");
 
 // create associations
 User.hasMany(Post, {
@@ -8,6 +9,25 @@ User.hasMany(Post, {
 
 Post.belongsTo(User, {
   foreignKey: "user_id",
+  onDelete: "SET NULL",
 });
 
-module.exports = { User, Post };
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+  onDelete: "SET NULL",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+module.exports = { User, Post, Comment };
