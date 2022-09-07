8cd932b9c2280449d3b89d9fee5f7c9372d68904
Sumit Ojha <sumit.ojha.dev@gmail.com>
2022-09-07 14:38:12 -0500

    Fixed the heroku log failure

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.use(session(sess));
 //use routes
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log(`Now Listening on ${PORT}`));
 });
